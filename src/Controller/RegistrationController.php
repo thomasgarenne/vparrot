b3cb5b5283c9cbb3f33d0d13c084f7c373d43bda
thomas <thomas.garenne@outlook.com>
2023-07-18 10:17:19 +0200

    change email address register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,7 +52,7 @@ class RegistrationController extends AbstractController
                 'app_verify_email',
                 $user,
                 (new TemplatedEmail())
-                    ->from(new Address('drivescares@mail.com', 'drivescares'))
+                    ->from(new Address('drivescares@gmail.com', 'drivescares'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')

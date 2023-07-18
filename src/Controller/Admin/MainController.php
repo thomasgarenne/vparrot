b6b2db662addf4f2a2f5147224bfed72ae04b398
thomas <thomas.garenne@outlook.com>
2023-07-18 11:02:47 +0200

    add info page admin

--- src/Controller/Admin/MainController.php
+++ src/Controller/Admin/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CommentsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +13,10 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(CommentsRepository $commentsRepository): Response
     {
-        return $this->render('admin/index.html.twig');
+        return $this->render('admin/index.html.twig', [
+            'comments' => $commentsRepository->findBy(['is_valid' => false], ['created_at' => 'DESC'])
+        ]);
     }
 }
